b0488170ee62da8fb058b218aa155870211588cb
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-11-16 11:40:13 +0000

    update

--- nop.c
+++ nop.c
@@ -4,6 +4,7 @@
  * nop - Does nothing.
  * @top: Pointer that points to the head pointer
  * @line_number: Line number where the opcode appears.
+ * Return: void
  */
 void nop(stack_t **top, unsigned int line_number)
 {
