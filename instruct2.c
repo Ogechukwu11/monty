ded547a390ae776f9e325035ab4e9e310819ba3d
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-11-16 23:27:43 +0000

    add two top elements

--- instruct2.c
+++ instruct2.c
@@ -30,3 +30,21 @@ void swap(stack_t **top, unsigned int line_number)
 
 	*top = element2;
 }
+
+/**
+ * add - To add top two elements of the stack.
+ * @top: A pointer to the pointer of the head.
+ * @line_number: Keeps track of the lines in the list.
+ * Return: void
+ */
+void add(stack_t **top, unsigned int line_number)
+{
+	if (*top == NULL || (*top)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*top)->next->n += (*top)->n;
+	pop(top, line_number);
+}
