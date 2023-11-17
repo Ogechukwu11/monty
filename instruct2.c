33632f4505bbd5984834cdfa09a55b046389358a
ademitidayo <adenijibimmy08@gmail.com>
2023-11-16 23:46:49 -0800

    control

--- instruct2.c
+++ instruct2.c
@@ -110,9 +110,15 @@ void mul_op(stack_t **stack, unsigned int line_number)
  */
 void mod_op(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL || (*stack)->n == 0)
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u:division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
