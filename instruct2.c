9e64c6638eabc5a81f4f12bd52a1036efed45138
ademitidayo <adenijibimmy08@gmail.com>
2023-11-16 18:14:08 -0800

    sub

--- instruct2.c
+++ instruct2.c
@@ -46,3 +46,21 @@ void add(stack_t **stack, unsigned int line_number)
 	(*stack)->next->n += (*stack)->n;
 	pop(stack, line_number);
 }
+/**
+ * sub - Subtracts the top element of the stack from the second top element.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number where the opcode appears.
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n -= (*stack)->n;
+
+	pop(stack, line_number);
+}
+
